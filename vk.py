ac1517c343c5c10d2df5103f730af446b32a3cbe
Paul <84198877+paul9956@users.noreply.github.com>
2022-06-07 20:46:12 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -3,33 +3,33 @@ from config import vk_token
 from datetime import datetime, date, timedelta, timezone
 from time import sleep
 
-
 while True:
     try:
-        def timedelta_to_hms(duration):
-            days, seconds = duration.days, duration.seconds
-            hours = days * 24 + seconds // 3600
-            minutes = (seconds % 3600) // 60
-            seconds = (seconds % 60)
-            return hours, minutes, seconds
-
-
         vk = VkApi(token=vk_token)
         counted_friends = len(vk.method("friends.getOnline"))
 
         delta = timedelta(hours=3, minutes=0)
         now_date = datetime.now(timezone.utc) + delta
-        format_date, format_date_ege = "%H:%M ● %d.%m.%Y", "%Y.%m.%d"
-
-        now_date_ege, ege_date = now_date.strftime(format_date_ege).split('.'), date(int(2022), int(6), int(20))    #Вместо 2022, 6, 20 поставь свой день
+        format_date, format_date_ege, format_minutes, format_hours = "%H:%M ● %d.%m.%Y", "%Y.%m.%d", "%M", "%H"
+        
+        now_date_ege, ege_date = now_date.strftime(format_date_ege).split('.'), date(int(2022), int(6), int(20))    #тут поменяй дату
         updated_date_ege = date(int(now_date_ege[0]), int(now_date_ege[1]), int(now_date_ege[2]))
-        hours, minutes, seconds = timedelta_to_hms(ege_date-updated_date_ege)
+        
+        after_date = 'Удачи на Егэ Крабоед'     #тут текст по истечению даты
+        ege = f'До ЕГЭ по Инфе {str(str(ege_date - updated_date_ege).split()[0])} дней'\
+            if int((str(ege_date - updated_date_ege).split()[0])) > 0 else after_date
 
+        hours = 24 * int((str(ege_date - updated_date_ege).split()[0])) - int(now_date.strftime(format_hours))
+        minutes = 60 - (int(datetime.now().strftime(format_minutes))) if hours > 0 else 0
+        
         vk.method("status.set", {"text": f"""● Сейчас: {now_date.strftime(format_date)}
-● Друзей онлайн: {str(counted_friends)}
-● До ЕГЭ по Инфе {str(str(ege_date-updated_date_ege).split()[0])} дней
-● Или {hours} часов {minutes+2} минут"""})
+● Друзей онлайн: {counted_friends}
+● {ege}
+● Или {hours} часов {minutes} минут"""})    
         sleep(30)
     except exceptions.Captcha as captcha:
-        VkApi(token=vk_token).captcha_handler(captcha)
+        def captcha_handler(captcha):
+            key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+            return captcha.try_again(key)
+        sleep(30)ken=vk_token).captcha_handler(captcha)
         sleep(30)
