3e77b16fd42041947e95302ecad2c49825e47013
Paul <84198877+paul9956@users.noreply.github.com>
2022-06-16 23:08:03 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -28,8 +28,4 @@ while True:
 ● Или {hours} часов {minutes} минут"""})    
         sleep(30)
     except exceptions.Captcha as captcha:
-        def captcha_handler(captcha):
-            key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
-            return captcha.try_again(key)
-        sleep(30)ken=vk_token).captcha_handler(captcha)
         sleep(30)
