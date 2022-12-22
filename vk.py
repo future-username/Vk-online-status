0b7785bc15d39f22a0d0efbcb1974235de6ea566
Paul <84198877+paul9956@users.noreply.github.com>
2022-12-22 22:31:14 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -12,7 +12,7 @@ while True:
         now_date = datetime.now(timezone.utc) + delta
         format_date, format_date_ege, format_minutes, format_hours = "%H:%M ● %d.%m.%Y", "%Y.%m.%d", "%M", "%H"
         
-        now_date_ege, ege_date = now_date.strftime(format_date_ege).split('.'), date(int(2022), int(6), int(20))    #тут поменяй дату
+        now_date_ege, ege_date = now_date.strftime(format_date_ege).split('.'), date(2022, 6, 20)    #тут поменяй дату
         updated_date_ege = date(int(now_date_ege[0]), int(now_date_ege[1]), int(now_date_ege[2]))
         
         after_date = 'Удачи на Егэ Крабоед'     #тут текст по истечению даты
